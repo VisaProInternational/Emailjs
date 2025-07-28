21e2e797b2e1d9d7beaf96d3d97fb495a112be71
VisaProInternational <visaprointernational@tuta.io>
2025-07-28 15:08:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,58 @@
-function sendMail(){
-    let parms = {
-        name : document.getElementById("name").vulue,
-        email : document.getElementById("email").vulue,
-        subject : document.getElementById("subject").vulue,
-    }
-    
-    emailjs.send("service_5slwirv","template_31o0qns",parms).then(alert("Email Sent!!"))
-}
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init({
+      publicKey: "LpjT6PFXqnLEJni6E",
+    });
+})();
+
+const googleSheetWebhookURL = "https://script.google.com/macros/s/AKfycbxkFO4k5Gz6hBRnpsEaPb-bKsS7_Rd6kSl6EZ2zx-9KICNrNHgADnOu0FphfMfl2ikq/exec";
+const msg = document.querySelector(".form-message");
+const loader = document.querySelector(".loader");
+
+window.onload = function() {
+    document.getElementById("contact-form").addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        loader.classList.add("show");
+        msg.innerHTML = "";
+        msg.classList.remove("show");
+
+        const form = this;
+
+        // ارسال ایمیل با EmailJS
+        emailjs.sendForm("service_4yvzeq8", "template_px597ka", form).then(
+            function() {
+                // ارسال داده به Google Sheets بعد از ارسال ایمیل موفق
+                const formData = new FormData(form);
+                const formDataObj = Object.fromEntries(formData.entries());
+
+                fetch(googleSheetWebhookURL, {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: new URLSearchParams(formDataObj)
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log("Data saved to Google Sheet:", data);
+                })
+                .catch(error => {
+                    console.error("Error saving to Google Sheet:", error);
+                });
+
+                loader.classList.remove("show");
+                msg.innerHTML = "<span class='success-msg'>Email Sent and Data Saved!</span>";
+                msg.classList.add("show");
+                form.reset();
+                setTimeout(() => msg.classList.remove("show"), 3000);
+            },
+            function(error){
+                loader.classList.remove("show");
+                msg.classList.add("show");
+                msg.innerHTML = "<span class='error-msg'>Email Not Sent</span>";
+                console.error("EmailJS error:", error);
+            }
+        );
+    });
+};
